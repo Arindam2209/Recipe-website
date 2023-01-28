55991b38ffa6f046bb74fa350cc620b5f060ce6e
Arindam Goswami <arindamgoswami2209@gmail.com>
2023-01-28 21:32:00 +0530

    Initial commit

---
+++ food-community-project/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <div className="footer">        
+        <p>Arindam Goswami's Foodary © 2023</p> 
+      </div>
+    );
+  }
+  export default Footer;
+
+
+
---
+++ food-community-project/src/recipe.js
@@ -0,0 +1,44 @@
+export const my_recipe = {
+    recipe_1: {
+      name: 'Chirashi Don',
+      preparation_time: '30 minutes',
+      servings: '2',
+      ingredients: [
+        '1/2 cup rice',
+        '1/2 cup water',
+        '2 eggs',
+        '300g of salmon, tuna and salmon roe',
+      ],
+      instructions: [
+        'Cook rice and water in a rice cooker',
+        'Fry eggs in a pan',
+        'Serve rice, eggs and fish on a plate',
+      ],
+      images: [
+        "/img/recipe1.jpg"
+      ]
+   
+    },
+
+    recipe_2: {
+        name: 'Indian Curry',
+        preparation_time: '45 minutes',
+        servings: '2',
+        ingredients: [
+          'Tumeric',
+          '1/2 cup water',
+          'tomatoes',
+          'vegetables and indian spices',
+        ],
+        instructions: [
+          'Make curry paste',
+          'Boil and add vegetables and spices',
+          'Serve rice, roti and curries on a plate',
+        ],
+        images: [
+            "/img/recipe2.jpg"
+          ]
+
+     
+      },
+  }
